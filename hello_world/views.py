4343de84e49818f0ed8a3769523253674372fb42
student81919 <kelaisplay@gmail.com>
2026-02-18 17:39:09 +0100

    Test CI: Wprowadzenie bledu

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Miłosz"
+moje_imie = "Błąd"
 msg = "Hello World!"
 
 
